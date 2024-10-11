943dc78b32bbda5f0f87ade3b079414ce4aaff0b
Luke Hide <luke.hide@gmail.com>
2024-10-11 13:45:54 +0200

    Removed animation on mobile

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -15,17 +15,17 @@ export default async function Home({
   return (
     <main className='relative flex w-full flex-col md:flex-row'>
       {/* Left Side Circles - hidden on small screens */}
-      <div className='hidden min-h-screen w-1/5 flex-row items-end pt-24 md:flex'>
+      <div className='hidden min-h-screen w-1/6 flex-row items-end pt-24 md:flex'>
         <Dots />
       </div>
 
       {/* Center-Left Text Content */}
-      <div className='relative z-10 flex min-h-screen w-full flex-col justify-start p-4 pt-20 text-left md:w-2/5 md:p-0 md:pt-40'>
+      <div className='relative z-10 flex min-h-screen w-full flex-col justify-start p-4 pt-20 text-left md:w-1/2 md:p-0 md:pt-40'>
         <HomepageText lang={lang} homepage={homepage} />
       </div>
 
       {/* Right Side Animation (Background) */}
-      <div className='absolute inset-0 -z-10 flex w-full flex-row items-center gap-10 md:relative md:inset-auto md:z-auto md:mt-12 md:w-2/5'>
+      <div className='inset-0 -z-10 md:flex w-full flex-row items-center gap-10 hidden md:relative md:inset-auto md:z-auto md:mt-12 md:w-2/6'>
         <Background />
         <div className='absolute inset-0 bg-gradient-to-t from-black/0 to-black/15 md:hidden'></div>
       </div>
